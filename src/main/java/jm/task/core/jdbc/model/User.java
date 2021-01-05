9a2817bfad275a4747efa80387d12e59f40b66d5
Artem Kulin <kulinartem@gmail.com>
2021-01-05 17:53:18 +0300

    edit annotation table name in entity User

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table (name = "test.User")
+@Table (name="User")
 public class User {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
