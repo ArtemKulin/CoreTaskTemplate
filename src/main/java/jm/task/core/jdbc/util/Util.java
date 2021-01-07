85236d4e246718d13ee3d94de64a100ba51c3143
Artem Kulin <kulinartem@gmail.com>
2021-01-07 15:55:34 +0300

    rework Util.class: add realization of connection to DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,25 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASS = "root";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static Connection connection = null;
 
-    public static void driverRegister () {
+    public static Connection getConnection() {
+        Properties settings = new Properties();
+        settings.put("user", USER);
+        settings.put("password", PASS);
         try {
             Class.forName(DRIVER);
-        } catch (ClassNotFoundException throwables) {
+            connection = DriverManager.getConnection(URL,settings);
+        } catch (ClassNotFoundException | SQLException throwables) {
             throwables.printStackTrace();
         }
+        return connection;
     }
 }
