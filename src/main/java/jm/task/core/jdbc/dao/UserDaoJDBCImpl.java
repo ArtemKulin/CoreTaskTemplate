49444bcea9a197c2f03aea91bfe46c18dc8a7c84
Artem Kulin <kulinartem@gmail.com>
2021-01-07 15:58:48 +0300

    rework UserDaoJDBCImpl.class: change realization for all methods

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,152 +9,78 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
-    private final String USER = "root";
-    private final String PASS = "root";
-    private final List<User> users = new ArrayList<>();
-    private Connection connection = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
+    private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
-        Util.driverRegister();
     }
 
     public void createUsersTable() {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            statement = connection.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS test.User (\n" +
-            "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS test.User (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
                     "  `name` VARCHAR(45) NULL,\n" +
                     "  `lastname` VARCHAR(45) NULL,\n" +
                     "  `age` INT NULL,\n" +
                     "  PRIMARY KEY (`id`),\n" +
                     "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);");
-            statement.executeBatch();
-
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {statement.close();}
-                if (connection != null) {connection.close();}
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
         }
     }
 
     public void dropUsersTable() {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            statement = connection.createStatement();
-            statement.execute("DROP TABLE IF EXISTS test.User;");
-            statement.executeBatch();
-            users.clear();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS test.User;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {statement.close();}
-                if (connection != null) {connection.close();}
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            preparedStatement = connection.prepareStatement("INSERT INTO test.User (name, lastname, age) VALUES (?,?,?)");
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.execute();
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO test.User (name, lastname, age) VALUES (?,?,?)")) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
             System.out.println("User " + name + " was added to DB!");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-                try {
-                    if (preparedStatement != null) {preparedStatement.close();}
-                    if (connection != null) {connection.close();}
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
+        }
     }
 
     public void removeUserById(long id) {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            preparedStatement = connection.prepareStatement("DELETE FROM test.User WHERE id = ?;");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
+        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM test.User WHERE id = ?;")){
+            statement.setLong(1, id);
+            statement.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-            try {
-                if (preparedStatement != null) {preparedStatement.close();}
-                if (connection != null) {connection.close();}
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
         }
-
     }
 
     public List<User> getAllUsers() {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM test.User");
+        List<User> users = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM test.User");
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
                 user.setName(resultSet.getString("name"));
                 user.setLastName(resultSet.getString("lastname"));
                 user.setAge(resultSet.getByte("age"));
-                if (users.stream().noneMatch(t -> t.equals(user))) {
-                    users.add(user);
-                }
+                users.add(user);
             }
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null) {resultSet.close();}
-                if (statement != null) {statement.close();}
-                if (connection != null) {connection.close();}
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
         }
         return users;
     }
 
     public void cleanUsersTable() {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            statement = connection.createStatement();
-            statement.execute("TRUNCATE TABLE test.User;");
-            statement.executeBatch();
+        try (Statement statement = connection.createStatement()){
+            statement.executeUpdate("TRUNCATE TABLE test.User;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
         }
     }
 }
