2ba79fb212f9baf5704a1d52402191b10b949606
Artem Kulin <kulinartem@gmail.com>
2021-01-08 14:33:57 +0300

    rework Util.class: add field DRIVER

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,6 +17,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
     private static final String USER = "root";
     private static final String PASS = "root";
+    private static final String DRIVER = "root";
     private static SessionFactory factory = null;
     private static Connection connection = null;
 
